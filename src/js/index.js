c629bb8597cba20350c197739f3e29d321fbe54e
uiuxkimabe <uiuxkimabe@gmail.com>
2023-10-09 00:13:32 +0700

    set price js

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,43 @@
 // Timbol Menu  Pada Index (u/ Menampilkan Fitur Lain)
 const btnMenu = document.getElementById("btnMenu");
 const nav = document.querySelector(".navigator");
-console.info(nav);
 btnMenu.addEventListener("click", () => {
   nav.classList.toggle("showUp");
   console.info("oke");
 });
 
+// Get bulan dan tanggal
+const pickMonth = new Date().getMonth(); // [0-11] (Januari - Desember)
+const pickDate = new Date().getDate();
+const pickDay = new Date().getDay(); // [0-6] (Minggu - Sabtu)
+
+// Array untuk menyesuaikan index dengan hari dan bulan
+let days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"]; // [0-6]
+let months = [
+  "Januari",
+  "Februari",
+  "Maret",
+  "April",
+  "Mei",
+  "Juni",
+  "Juli",
+  "Agustus",
+  "September",
+  "Oktober",
+  "November",
+  "Desember",
+]; // [0-11]
+
+// Harga Modal
+class hargaModal {
+  prPrice; // Harga Modal Premium
+  mdPrice; // Harga Modal Medium
+  lgPrice; // Harga Modal Large
+  //   Method Premium Villa
+  premiumWd() {
+    let total = this.prPrice + 10 + "%";
+    return total;
+  }
+}
+
+const testPrice = new hargaModal();
