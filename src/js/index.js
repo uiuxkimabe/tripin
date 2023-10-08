f4fbf3cb08a4bbeca796e8234333b179a7670472
uiuxkimabe <uiuxkimabe@gmail.com>
2023-10-08 16:55:56 +0700

    adding photo villa

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 /*? no js js needed from me */
+
+// Timbol Menu  Pada Index (u/ Menampilkan Fitur Lain)
+const btnMenu = document.getElementById("btnMenu");
+const nav = document.querySelector(".navigator");
+console.info(nav);
+btnMenu.addEventListener("click", () => {
+  nav.classList.toggle("showUp");
+  console.info("oke");
+});
+
