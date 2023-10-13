9d167047ff5c937835f9fc48e82836175ba26646
uiuxkimabe <uiuxkimabe@gmail.com>
2023-10-13 19:56:42 +0700

    add filter and villa

--- src/js/index.js
+++ src/js/index.js
@@ -12,12 +12,64 @@ btnMenu.addEventListener("click", () => {
 const filterVilla = document.querySelectorAll(".filter-villa ul li");
 const listVilla = document.querySelectorAll("main .card-rooms");
 
-function filter() {
-  for (let i = 0; i < filterVilla.length; i++) {
-    filterVilla[i].classList.toggle("active");
-  }
-}
+// destructure filter ville
+const [all, premium, medium, large] = filterVilla;
 
-filterVilla[0].addEventListener("click", () => {
-  filter();
+console.info(filterVilla, listVilla);
+
+listVilla.forEach((element) => {
+  all.addEventListener("click", () => {
+    element.style.display = element.classList.contains("all") ? "grid" : "none";
+    all.style.background = "black";
+    all.style.color = "white";
+    premium.style.background = "transparent";
+    premium.style.color = "black";
+    medium.style.background = "transparent";
+    medium.style.color = "black";
+    large.style.background = "transparent";
+    large.style.color = "black";
+    console.info("all");
+  });
+  premium.addEventListener("click", () => {
+    element.style.display = element.classList.contains("premium")
+      ? "grid"
+      : "none";
+    all.style.background = "transparent";
+    all.style.color = "black";
+    premium.style.background = "black";
+    premium.style.color = "white";
+    medium.style.background = "transparent";
+    medium.style.color = "black";
+    large.style.background = "transparent";
+    large.style.color = "black";
+    console.info("premium");
+  });
+  medium.addEventListener("click", () => {
+    element.style.display = element.classList.contains("medium")
+      ? "grid"
+      : "none";
+    all.style.background = "transparent";
+    all.style.color = "black";
+    premium.style.background = "transparent";
+    premium.style.color = "black";
+    medium.style.background = "black";
+    medium.style.color = "white";
+    large.style.background = "transparent";
+    large.style.color = "black";
+    console.info("medium");
+  });
+  large.addEventListener("click", () => {
+    element.style.display = element.classList.contains("large")
+      ? "grid"
+      : "none";
+    all.style.background = "transparent";
+    all.style.color = "black";
+    premium.style.background = "transparent";
+    premium.style.color = "black";
+    medium.style.background = "transparent";
+    medium.style.color = "black";
+    large.style.background = "black";
+    large.style.color = "white";
+    console.info("large");
+  });
 });
--- src/js/price.js
+++ src/js/price.js
@@ -25,11 +25,96 @@ class hargaModal {
   prPrice; // Harga Modal Premium
   mdPrice; // Harga Modal Medium
   lgPrice; // Harga Modal Large
+  percent; // Percentase Margin
+  feeGuide = 150000; // fee untuk guider
   //   Method Premium Villa
   premiumWd() {
-    let total = this.prPrice + 10 + "%";
+    let total = this.prPrice * this.percent + this.feeGuide;
+    return total;
+  }
+  mediumWd() {
+    let total = this.mdPrice * this.percent + this.feeGuide;
+    return total;
+  }
+  largeWd() {
+    let total = this.lgPrice * this.percent + this.feeGuide;
     return total;
   }
 }
 
-const testPrice = new hargaModal();
+// Villa Ajap
+{
+  const villaAjapPrice = document.querySelector(".ajap .price");
+  const villaAjap = new hargaModal();
+  villaAjap.mdPrice = 1500000;
+  villaAjap.percent = 1.66;
+  let priceShow = villaAjap.mediumWd();
+  villaAjapPrice.innerHTML = priceShow.toLocaleString("id-ID");
+  console.info(villaAjap.mediumWd());
+}
+
+// Santoria Villa
+{
+  const santoriaVillaPrice = document.querySelector(".santoria .price");
+  const santoriaVilla = new hargaModal();
+  santoriaVilla.mdPrice = 1500000;
+  santoriaVilla.percent = 1.66;
+  let priceShow = santoriaVilla.mediumWd();
+  santoriaVillaPrice.innerHTML = priceShow.toLocaleString("id-ID");
+  console.info(santoriaVilla.mediumWd());
+}
+
+// Konoha Villa
+{
+  const konohaVillaPrice = document.querySelector(".konoha .price");
+  const konohaVilla = new hargaModal();
+  konohaVilla.mdPrice = 2000000;
+  konohaVilla.percent = 1.66;
+  let priceShow = konohaVilla.mediumWd();
+  konohaVillaPrice.innerHTML = priceShow.toLocaleString("id-ID");
+  console.info(konohaVilla.mediumWd());
+}
+
+// Bukit Indah Villa
+{
+  const bukitIndahPrice = document.querySelector(".bukitIndah .price");
+  const bukitIndah = new hargaModal();
+  bukitIndah.mdPrice = 6000000;
+  bukitIndah.percent = 1;
+  let priceShow = bukitIndah.mediumWd();
+  bukitIndahPrice.innerHTML = priceShow.toLocaleString("id-ID");
+  console.info(bukitIndah.mediumWd());
+}
+
+// Kiki Villa
+{
+  const kikiPrice = document.querySelector(".kiki .price");
+  const kiki = new hargaModal();
+  kiki.mdPrice = 3000000;
+  kiki.percent = 1;
+  let priceShow = kiki.mediumWd();
+  kikiPrice.innerHTML = priceShow.toLocaleString("id-ID");
+  console.info(kiki.mediumWd());
+}
+
+// Cindy Villa
+{
+  const cindyPrice = document.querySelector(".cindy .price");
+  const cindy = new hargaModal();
+  cindy.mdPrice = 3000000;
+  cindy.percent = 1;
+  let priceShow = cindy.mediumWd();
+  cindyPrice.innerHTML = priceShow.toLocaleString("id-ID");
+  console.info(cindy.mediumWd());
+}
+
+// Arif Villa
+{
+  const arifPrice = document.querySelector(".arif .price");
+  const arif = new hargaModal();
+  arif.mdPrice = 3000000;
+  arif.percent = 1;
+  let priceShow = arif.mediumWd();
+  arifPrice.innerHTML = priceShow.toLocaleString("id-ID");
+  console.info(arif.mediumWd());
+}
