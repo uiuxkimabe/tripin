b487c27dcfb25448b8fc24263d51a55581996e37
uiuxkimabe <uiuxkimabe@gmail.com>
2023-10-16 08:31:26 +0700

    slider photo

--- src/js/price.js
+++ src/js/price.js
@@ -32,7 +32,13 @@ class hargaModal {
     let total = this.prPrice * this.percent + this.feeGuide;
     return total;
   }
+  deposite() {
+    let minDeposite = this.prPrice / 2;
+    return minDeposite;
+  }
 }
+
+const dp = document.querySelector(".downpayment .deposite");
 
 // Villa Ajap
 {
@@ -42,71 +48,7 @@ class hargaModal {
   villaAjap.percent = 1.66;
   let priceShow = villaAjap.premiumWd();
   villaAjapPrice.innerHTML = priceShow.toLocaleString("id-ID");
-  console.info(villaAjap.premiumWd());
-}
-
-// Santoria Villa
-{
-  const santoriaVillaPrice = document.querySelector(".santoria .price");
-  const santoriaVilla = new hargaModal();
-  santoriaVilla.prPrice = 1500000;
-  santoriaVilla.percent = 1.66;
-  let priceShow = santoriaVilla.premiumWd();
-  santoriaVillaPrice.innerHTML = priceShow.toLocaleString("id-ID");
-  console.info(santoriaVilla.premiumWd());
-}
-
-// Konoha Villa
-{
-  const konohaVillaPrice = document.querySelector(".konoha .price");
-  const konohaVilla = new hargaModal();
-  konohaVilla.prPrice = 2000000;
-  konohaVilla.percent = 1.66;
-  let priceShow = konohaVilla.premiumWd();
-  konohaVillaPrice.innerHTML = priceShow.toLocaleString("id-ID");
-  console.info(konohaVilla.premiumWd());
-}
-
-// Bukit Indah Villa
-{
-  const bukitIndahPrice = document.querySelector(".bukitIndah .price");
-  const bukitIndah = new hargaModal();
-  bukitIndah.prPrice = 6000000;
-  bukitIndah.percent = 1;
-  let priceShow = bukitIndah.premiumWd();
-  bukitIndahPrice.innerHTML = priceShow.toLocaleString("id-ID");
-  console.info(bukitIndah.premiumWd());
-}
-
-// Kiki Villa
-{
-  const kikiPrice = document.querySelector(".kiki .price");
-  const kiki = new hargaModal();
-  kiki.prPrice = 3000000;
-  kiki.percent = 1;
-  let priceShow = kiki.premiumWd();
-  kikiPrice.innerHTML = priceShow.toLocaleString("id-ID");
-  console.info(kiki.premiumWd());
-}
-
-// Cindy Villa
-{
-  const cindyPrice = document.querySelector(".cindy .price");
-  const cindy = new hargaModal();
-  cindy.prPrice = 3000000;
-  cindy.percent = 1;
-  let priceShow = cindy.premiumWd();
-  cindyPrice.innerHTML = priceShow.toLocaleString("id-ID");
-  console.info(cindy.premiumWd());
-}
-
-// Arif Villa
-{
-  const arifPrice = document.querySelector(".arif .price");
-  const arif = new hargaModal();
-  arif.prPrice = 3000000;
-  arif.percent = 1;
-  let priceShow = arif.premiumWd();
-  arifPrice.innerHTML = priceShow.toLocaleString("id-ID");
-  console.info(arif.premiumWd());
+  let minDp = priceShow / 2;
+  dp.value = minDp.toLocaleString("id-ID");
+  console.info(priceShow, minDp);
 }
