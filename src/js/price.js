5d2ca3da84cc3f858d61eada7a89b8152e18b3d6
uiuxkimabe <uiuxkimabe@gmail.com>
2023-10-21 20:24:14 +0700

    price show in index and detail

--- src/js/price.js
+++ src/js/price.js
@@ -42,3 +42,43 @@ class hargaModal {
 }
 
 const dp = document.querySelector(".downpayment .deposite");
+
+
+// Villa Ajap
+{
+  const villaAjapPriceWd = document.querySelector("#ajap .price-wd");
+  const villaAjapPriceWe = document.querySelector("#ajap .price-we");
+  const villaAjap = new hargaModal();
+  villaAjap.basicPrice = 1500000;
+  villaAjap.percentWd = 1.66;
+  villaAjap.percentWe = 2;
+  let priceShowWd = villaAjap.priceWd();
+  let priceShowWe = villaAjap.priceWe();
+
+  // Tampilan di halaman beranda
+  villaAjapPriceWd.innerHTML = priceShowWd.toLocaleString("id-ID");
+  villaAjapPriceWe.innerHTML = priceShowWe.toLocaleString("id-ID");
+  let minDp = priceShowWd / 2;
+  console.info(priceShowWd, priceShowWe);
+  // dp.value = minDp.toLocaleString("id-ID");
+}
+
+
+// Villa Erni
+{
+  const villaErniPriceWd = document.querySelector("#erni .price-wd");
+  const villaErniPriceWe = document.querySelector("#erni .price-we");
+  const villaErni = new hargaModal();
+  villaErni.basicPrice = 1000000;
+  villaErni.percentWd = 1.66;
+  villaErni.percentWe = 2.9;
+  let priceShowWd = villaErni.priceWd();
+  let priceShowWe = villaErni.priceWe();
+
+  // Tampilan di halaman beranda
+  villaErniPriceWd.innerHTML = priceShowWd.toLocaleString("id-ID");
+  villaErniPriceWe.innerHTML = priceShowWe.toLocaleString("id-ID");
+  let minDp = priceShowWd / 2;
+  console.info(priceShowWd, priceShowWe);
+  // dp.value = minDp.toLocaleString("id-ID");
+}
