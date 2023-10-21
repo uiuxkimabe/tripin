7e415d0b929d9843eba25a04c8f7ebfcdb44b8f1
uiuxkimabe <uiuxkimabe@gmail.com>
2023-10-21 18:55:29 +0700

    carousel

--- src/js/detail-villa.js
+++ src/js/detail-villa.js
@@ -1,3 +1,58 @@
+// slider detail villa
+const imgPreview = document.querySelector(".img-preview");
+const slider = document.querySelector(".slider");
+const sliderPhoto = document.querySelectorAll(".slider .slider-photo");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const closeBtn = document.getElementById("close");
+imgPreview.addEventListener("click", () => {
+  slider.classList.add("popUp");
+  setTimeout(() => {
+    sliderPhoto.forEach((element) => {
+      element.style.transform = "scale(1)";
+    });
+  }, 500);
+});
+closeBtn.addEventListener("click", () => {
+  slider.classList.remove("popUp");
+});
+
+let count = 0;
+console.info(count);
+const size = sliderPhoto[0].clientWidth;
+
+function caraousel() {
+  sliderPhoto.forEach((element) => {
+    element.style.transform = "translateX(" + -size * count + "px)";
+  });
+}
+
+nextBtn.addEventListener("click", () => {
+  count++;
+  console.info(count);
+  caraousel();
+  if (count == 1) {
+    prevBtn.disabled = false;
+    prevBtn.style.color = "#fff";
+  } else if (count == sliderPhoto.length - 1) {
+    nextBtn.disabled = true;
+    nextBtn.style.color = "#898989";
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  count--;
+  console.info(count);
+  caraousel();
+  if (count == 0) {
+    prevBtn.disabled = true;
+    prevBtn.style.color = "#898989";
+  } else if (sliderPhoto.length - 1) {
+    nextBtn.disabled = false;
+    nextBtn.style.color = "#fff";
+  }
+});
+
 // Get bulan dan tanggal
 const pickMonth = new Date().getMonth(); // [0-11] (Januari - Desember)
 const pickDate = new Date().getDate();
@@ -61,58 +116,3 @@ const dp = document.querySelector(".downpayment .deposite");
   console.info(priceShowWd, priceShowWe);
   dp.value = minDp.toLocaleString("id-ID");
 }
-
-// slider detail villa
-const imgPreview = document.querySelector(".img-preview");
-const slider = document.querySelector(".slider");
-const sliderPhoto = document.querySelectorAll(".slider .slider-photo");
-const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("next");
-const closeBtn = document.getElementById("close");
-imgPreview.addEventListener("click", () => {
-  slider.classList.add("popUp");
-  setTimeout(() => {
-    sliderPhoto.forEach((element) => {
-      element.style.transform = "scale(1)";
-    });
-  }, 500);
-});
-closeBtn.addEventListener("click", () => {
-  slider.classList.remove("popUp");
-});
-
-let count = 0;
-console.info(count);
-const size = sliderPhoto[0].clientWidth;
-
-function caraousel() {
-  sliderPhoto.forEach((element) => {
-    element.style.transform = "translateX(" + -size * count + "px)";
-  });
-}
-
-nextBtn.addEventListener("click", () => {
-  count++;
-  console.info(count);
-  caraousel();
-  if (count == 1) {
-    prevBtn.disabled = false;
-    prevBtn.style.color = "#fff";
-  } else if (count == sliderPhoto.length - 1) {
-    nextBtn.disabled = true;
-    nextBtn.style.color = "#898989";
-  }
-});
-
-prevBtn.addEventListener("click", () => {
-  count--;
-  console.info(count);
-  caraousel();
-  if (count == 0) {
-    prevBtn.disabled = true;
-    prevBtn.style.color = "#898989";
-  } else if (sliderPhoto.length - 1) {
-    nextBtn.disabled = false;
-    nextBtn.style.color = "#fff";
-  }
-});
